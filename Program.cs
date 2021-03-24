81ec92b1f4da7b7eedcf9cb0d6b80cf79d7c2a94
Rodrigo Xavier <81274798+rodrigo-xavi@users.noreply.github.com>
2021-03-24 00:22:22 -0300

    Add files via upload
    Manipulação de vetores com Random POO.
    Informar maior vetor de 20 posições, maior e menor numero do vetor, primeira ocorrência de x no vetor, posições das ocorrências de x, e quantidade de ocorrências de x.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04_RandomVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor v = new Vetor(20);
+            int x;
+           
+            v.imprimirVetor();
+
+            Console.WriteLine("\nDIGITE O VALOR X: ");
+            x = int.Parse(Console.ReadLine());
+           
+            v.MaiorMenor();
+            v.PrimeiraOcorrencia(x);
+            v.PosiçoesOcorrencia(x);
+            v.numerodeOcorrencias(x);
+
+
+
+        }
+
+    }
+}
---
+++ Vetor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_RandomVetores
+{
+    class Vetor
+    {
+        
+        private int[] v;
+        public Vetor (int x)
+        {
+           gerarVetor(x);
+            
+        }
+        
+        public void gerarVetor(int x )
+        {
+            this.v = new int[x];
+            Random ale = new Random();
+            
+
+            for(int i = 0; i < v.Length; i++)
+            {
+                v[i] = ale.Next(1, 10);
+            }
+
+        }
+
+        public void imprimirVetor()
+        {
+            Console.WriteLine("---------------------------- VETOR -----------------------------------");
+            for(int i = 0; i < v.Length; i++)
+            {
+                Console.Write(" "+v[i]+" ");
+            }
+        }
+
+        public void MaiorMenor()
+        {
+            int maior = 0;
+            int menor = 0;
+            Console.WriteLine(" \n ----------------- MAIOR E MENOR VALOR DO VETOR ------------------");
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] > v[maior])
+
+                {
+                    maior = i;
+
+                }
+            }
+            Console.WriteLine("MAIOR VALOR DO VETOR: " + v[maior]);
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] < v[menor])
+
+                {
+                    menor = i;
+
+                }
+            }
+            Console.WriteLine("MENOR VALOR DO VETOR: " + v[menor]);
+        }
+
+
+        public void PrimeiraOcorrencia( int p)
+        {
+            int i;
+            Console.WriteLine(" \n----------------- POSIÇÃO DA PRIMEIRA OCORRÊNCIA DE X ------------------");
+            for (i = 0; i < v.Length; i++)
+            {
+                if (v[i] == p)
+
+                {
+                    break;
+
+                }
+            }
+            if (i < v.Length)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                i = -1;
+
+                Console.WriteLine("valor não encontrado");
+            }
+        }
+
+        public void PosiçoesOcorrencia( int p)
+        {
+            int i ;
+           
+            Console.WriteLine(" \n----------------- POSIÇÕES DAS OCORRÊNCIA DE X ------------------");
+           
+            for (i = 0 ; i < v.Length; i++)
+            {
+                    if (v[i] == p)
+                    {
+                    
+                      Console.WriteLine(i);
+                    }
+            }
+        }
+
+       public void numerodeOcorrencias(int p)
+        {
+            int cont=0, i;
+            Console.WriteLine(" \n----------------- NUMERO DE OCORRÊNCIA DE X ------------------");
+            for (i = 0; i < v.Length; i++)
+            {
+                if (v[i] == p)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine("Quantidade de ocorrencias do elemento X no vetor: "+cont);
+            
+        }
+       
+    }
+}
